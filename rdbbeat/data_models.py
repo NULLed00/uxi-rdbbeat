30692c210338f52a095f5c7893322bba273c5699
ignacioarce2 <ignacio.arce@datos101.com>
2026-01-22 14:53:48 +0100

    Adapting the Schedule class to allow for */T expressions in the celery beat schedules (which are natively supported) but not covered by the original validators

--- rdbbeat/data_models.py
+++ rdbbeat/data_models.py
@@ -12,50 +12,47 @@ class Schedule(BaseModel):
     month_of_year: str = "*"
     timezone: str = "UTC"
 
-    @validator("minute")
-    def minute_validation(cls, v: str) -> str:  # noqa: D102, N805
+    @staticmethod
+    def check_data_range(v, _min, _max, error_message):
+        assert int(v) >= _min and int(v) < _max, error_message  # noqa: PLR2004, PT018, S101
+
+    @staticmethod
+    def validate(v, _min, _max, error_message, _type):
         if v == "*":
             return v
-        if not v.isdigit():
-            raise ValueError(f"Minute: '{v}' is not a valid int")  # noqa: EM102, TRY003
-        assert int(v) >= 0 and int(v) < 60, "Minute value must range between 0 and 59"  # noqa: PLR2004, PT018, S101
+
+        elif v.startswith('*/'):
+            Schedule.check_data_range(v.strip('*/'), _min, _max, error_message)
+
+        elif not v.isdigit():
+            raise ValueError(f"{_type}: '{v}' is not a valid int")  # noqa: EM102, TRY003
+
+        else:
+            Schedule.check_data_range(v, _min, _max, error_message)
+
         return v
+
+    @validator("minute")
+    def minute_validation(cls, v: str) -> str:  # noqa: D102, N805
+        return cls.validate(v, 0, 60, "Minute value must range between 0 and 59", "Minute")
 
     @validator("hour")
     def hour_validation(cls, v: str) -> str:  # noqa: D102, N805
-        if v == "*":
-            return v
-        if not v.isdigit():
-            raise ValueError(f"Hour: '{v}' is not a valid int")  # noqa: EM102, TRY003
-        assert int(v) >= 0 and int(v) < 24, "Hour value must range between 0 and 23"  # noqa: PLR2004, PT018, S101
-        return v
+        return cls.validate(v, 0, 24, "Hour value must range between 0 and 23", "Hour")
+
 
     @validator("day_of_week")
     def day_of_week_validation(cls, v: str) -> str:  # noqa: D102, N805
-        if v == "*":
-            return v
-        if not v.isdigit():
-            raise ValueError(f"Day of week: '{v}' is not a valid int")  # noqa: EM102, TRY003
-        assert int(v) >= 0 and int(v) < 7, "Day of the week value must range between 0 and 6"  # noqa: PLR2004, PT018, S101
-        return v
+        return cls.validate(v, 0, 7, "Day of the week value must range between 0 and 6", "Day of week")
+
 
     @validator("day_of_month")
     def day_of_month_validation(cls, v: str) -> str:  # noqa: D102, N805
-        if v == "*":
-            return v
-        if not v.isdigit():
-            raise ValueError(f"Day of month: '{v}' is not a valid int")  # noqa: EM102, TRY003
-        assert int(v) > 0 and int(v) < 32, "Day of the month value must range between 1 and 31"  # noqa: PLR2004, PT018, S101
-        return v
+        return cls.validate(v, 0, 32, "Day of the month value must range between 1 and 31", "Day of the month")
 
     @validator("month_of_year")
     def month_of_year_validation(cls, v: str) -> str:  # noqa: D102, N805
-        if v == "*":
-            return v
-        if not v.isdigit():
-            raise ValueError(f"Month: '{v}' is not a valid int")  # noqa: EM102, TRY003
-        assert int(v) > 0 and int(v) < 13, "Month of year value must range between 0 and 12"  # noqa: PLR2004, PT018, S101
-        return v
+        return cls.validate(v, 0, 13, "Month of year value must range between 0 and 12", "Month of the year")
 
 
 class ScheduledTask(BaseModel):
