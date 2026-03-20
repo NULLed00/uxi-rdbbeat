c7993cab4a5c7487488939d6159090ebc3ada893
ignacioarce2 <ignacio.arce@datos101.com>
2026-03-20 10:46:49 +0100

    Adding DB URI to settings using secure secrets rather than an evironment variable

--- rdbbeat/settings.py
+++ rdbbeat/settings.py
@@ -1,9 +1,20 @@
 import os
-
 from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.environ.get(
-    "DATABASE_URL", "postgresql://username:password@localhost:5432/database"
-)
+def get_secret(key:str):
+
+    # Check for _FILE suffix first
+    file_env = f"{key}_FILE"
+
+    if file_env in os.environ:
+        with open(os.environ[file_env], 'r') as f:
+            return f.read().strip()
+
+
+DB_PASSWORD = get_secret('DB_PASSWORD')
+DB_USER = os.getenv('DB_USER', 'vigilia')
+DEFAULT_DATABASE = os.getenv('DATABASE_NAME', 'vigilia')
+DB_IP_AND_PORT = os.getenv('DB_IP_AND_PORT', 'vigilia-db:5432')
+DATABASE_URL =  f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_IP_AND_PORT}/{DEFAULT_DATABASE}'
